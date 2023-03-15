f889a41f903fb57b11c3532d725292b98d735118
theflag505 <hamdan.mobeen@gmail.com>
2023-03-14 22:50:03 -0700

    FA 122 Login Legacy Features implemented

--- appLegacyFragment/src/main/java/com/hamdan/forzenbook/legacy/fragment/app/view/LegacyFragmentActivity.kt
+++ appLegacyFragment/src/main/java/com/hamdan/forzenbook/legacy/fragment/app/view/LegacyFragmentActivity.kt
@@ -3,11 +3,13 @@ package com.hamdan.forzenbook.legacy.fragment.app.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.hamdan.forzenbook.legacy.fragment.app.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LegacyFragmentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_legacy_fragment)
-        // Todo adjust anything needed here in FA-122 / 24
+        // Todo adjust anything needed here in FA-126, though there shouldn't be
     }
 }
